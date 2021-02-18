e5554dec44310156dead0750dc3ce0840e989951
chaplygindenys <chaplygindenys@gmail.com>
2021-02-18 18:39:39 +0200

    add https://chaplygindenys.github.io/soap/

--- client.php
+++ client.php
@@ -2,7 +2,7 @@
 try{
     //make soap client
     $client = new SoapClient(
-        "http://stock.wsdl");//global link to .wsld server
+        "https://chaplygindenys.github.io/soap/");//global link to .wsld server
     $result = $client->getStock("3");
     echo "текуший запас на складе:", $result;
 
